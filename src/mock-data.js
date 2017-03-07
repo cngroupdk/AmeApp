eb1995648b8ea858f2ca74378b39e50231fd2b7d
Robert Szlezak <szlezak@cngroup.dk>
2017-03-07 10:24:00 +0100

    Created mock data

---
+++ src/mock-data.js
@@ -0,0 +1,33 @@
+export const messages = [
+  {
+    reactions: [
+      {
+        count: 1,
+        name: "grin",
+      }
+    ],
+    text: "Pani je premotivovana, keby ste chceli vediet, tak v sobotu o 23:00 odomna pytala logo firmy ASAP. :smile:",
+    ts: "1488812114.973537",
+    type: "message",
+    user: "U4E7EL1KN"
+  },
+  {
+    reactions: [
+      {
+        count: 1,
+        name: "+1",
+      }
+    ],
+    text: "Cilem workshopu bude vytvorit aplikaci A’me - appreciate me - ktera slouzi k gamifikaci prostredi slacku :slightly_smiling_face:",
+    ts: "1488805713.661555",
+    type: "message",
+    user: "U4E4ME9V0"
+  },
+  {
+    text: "xxx",
+    ts: "1488805099.640110",
+    type: "message",
+    user: "U4DE4DY01"
+  }
+];
+
