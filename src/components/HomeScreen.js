47b3f4abc04948e98aaba4999df0ae1aa7808544
Robert Szlezak <szlezak@cngroup.dk>
2017-03-11 23:18:50 +0100

    Added style constants

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -8,49 +8,57 @@ import {
 
 import Icon from 'react-native-vector-icons/FontAwesome';
 
+import colorContstants from '../helpers/color-constants';
 import { getAllUsers, getAllChannels, getChannelsHistory } from '../helpers/backend';
 
 const styles = StyleSheet.create({
   homeContainer: {
     flex: 1,
-    backgroundColor: '#e6f8fc',
+    backgroundColor: colorContstants.colorFoam,
   },
   homeHeader: {
    marginTop: 20,
-   backgroundColor: '#ed1848',
+   backgroundColor: colorContstants.colorCrimson,
    alignItems: 'center',
   },
   homeCellContainer: {
     flexDirection: 'column',
     alignItems: 'center',
-    backgroundColor: '#ffffff',
+    backgroundColor: colorContstants.colorWhite,
     paddingTop: 20,
     paddingBottom: 20,
     borderTopWidth: StyleSheet.hairlineWidth,
-    borderColor: '#e6f8fc',
+    borderColor: colorContstants.colorFoam,
   },
   homeCellTitle: {
     fontSize: 22,
     fontWeight: '700',
     paddingBottom: 10,
     paddingTop: 10,
-    color: '#ed1848',
+    color: colorContstants.colorCrimson,
   },
   homeCellText: {
     fontSize: 20,
     fontWeight: '700',
-    color: '#073453',
+    color: colorContstants.colorTarawera,
   },
 });
 
 class HomeScreen extends Component {
+  static navigationOptions = {
+    tabBar: {
+      icon: ({ tintColor }) => (
+        <Icon name='home' size={28} color={tintColor} />
+      ),
+    },
+  }
+
   constructor(props) {
     super(props);
 
     this._getTotalUsers = this._getTotalUsers.bind(this);
     this._getTotalChannels = this._getTotalChannels.bind(this);
     this._getTotalMessages = this._getTotalMessages.bind(this);
-    this._renderHomeCell = this._renderHomeCell.bind(this);
 
     this.state = { totalUsers: 0, totalChannels: 0, totalMessages: 0 };
   }
@@ -73,40 +81,41 @@ class HomeScreen extends Component {
     this.setState({ totalMessages: messages.length });
   }
 
-  _renderHomeCell(prop) {
-    const { count, icon, titile } = prop;
-
+  _renderHomeCell(prop, idx) {
+    const { countId, icon, title } = prop;
+    console.log(countId, icon, title)
+    console.log(this.state)
     return (
-      <View style={styles.homeCellContainer}>
-        <Icon name={icon} size={30} color='#98d0da' />
+      <View key={idx} style={styles.homeCellContainer}>
+        <Icon name={icon} size={30} color={colorContstants.colorAquaIsland} />
         <Text style={styles.homeCellTitle}>{title}</Text>
-        <Text style={styles.homeCellText}>{count}</Text>
+        <Text style={styles.homeCellText}>{this.state[countId]}</Text>
       </View>
     );
   }
 
   render() {
-    let homeProps = [
+    const homeProps = [
       {
         icon: 'users',
         title: 'Total Users',
-        count: this.state.totalUsers,
+        countId: 'totalUsers',
       },
       {
         icon: 'flag',
         title: 'Total Channels',
-        count: this.state.totalChannels,
+        countId: 'totalChannels',
       },
       {
         icon: 'comment',
         title: 'Total Messages',
-        count: this.state.totalMessages,
+        countId: 'totalMessages',
       },
     ];
 
     return (
       <ScrollView style={styles.homeContainer}>
-        {homeProps.map((prop) => {this._renderHomeCell})}
+        {homeProps.map((prop, idx) => {this._renderHomeCell(prop, idx)})}
       </ScrollView>
     );
   }
---
+++ src/helpers/color-constants.js
@@ -0,0 +1,9 @@
+const colorContstants = {
+  colorAquaIsland: '#98D0DA',
+  colorCrimson: '#ED1848',
+  colorFoam: '#E6F8FC',
+  colorTarawera: '#073453',
+  colorWhite: '#FFFFFF',
+};
+
+export default colorContstants;
