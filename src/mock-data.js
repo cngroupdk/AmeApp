6b85d4fe6cd508febb697d707682c8e186bbaace
lenkavon <lenka.vondr@gmail.com>
2017-03-18 07:57:57 +0100

    update mockdata

--- src/mock-data.js
+++ src/mock-data.js
@@ -3,31 +3,43 @@ export const messages = [
     reactions: [
       {
         count: 1,
-        name: "grin",
-      }
+        name: 'grin',
+      },
     ],
-    text: "Pani je premotivovana, keby ste chceli vediet, tak v sobotu o 23:00 odomna pytala logo firmy ASAP. :smile:",
-    ts: "1488812114.973537",
-    type: "message",
-    user: "U4E7EL1KN"
+    text: 'Pani je premotivovana, keby ste chceli vediet, tak v sobotu o 23:00 odomna pytala logo firmy ASAP. :smile:',
+    ts: '1488812114.973537',
+    type: 'message',
+    user: 'U4E7EL1KN',
   },
   {
     reactions: [
       {
         count: 1,
-        name: "+1",
-      }
+        name: '+1',
+      },
     ],
-    text: "Cilem workshopu bude vytvorit aplikaci A’me - appreciate me - ktera slouzi k gamifikaci prostredi slacku :slightly_smiling_face:",
-    ts: "1488805713.661555",
-    type: "message",
-    user: "U4E4ME9V0"
+    text: 'Cilem workshopu bude vytvorit aplikaci A’me - appreciate me - ktera slouzi k gamifikaci prostredi slacku :slightly_smiling_face:',
+    ts: '1488805713.661555',
+    type: 'message',
+    user: 'U4E4ME9V0',
   },
   {
-    text: "xxx",
-    ts: "1488805099.640110",
-    type: "message",
-    user: "U4DE4DY01"
-  }
+    text: 'xxx',
+    ts: '1488805099.640110',
+    type: 'message',
+    user: 'U4DE4DY01',
+  },
 ];
 
+const user = {
+  id: 'U4E4ME9V0',
+  profile: {
+    first_name: 'Lenka',
+    last_name: 'Vondrackova',
+    avatar_hash: 'a2e0e9ed067c',
+    image_48: 'https://avatars.slack-edge.com/2017-03-06/150162136594_a2e0e9ed067c67ab26b3_48.jpg',
+    real_name: 'Lenka Vondrackova',
+    real_name_normalized: 'Lenka Vondrackova',
+    email: 'vondrackova@cngroup.dk',
+  },
+};
